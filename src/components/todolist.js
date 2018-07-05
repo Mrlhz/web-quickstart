d41de2c00487258a2a4280ef5fc081b394c649d7
lhz <mr__lhz@163.com>
2018-07-06 01:31:21 +0800

    使用redux完成添加todo item功能

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -6,8 +6,31 @@ export default class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = store.getState();
-        console.log(this.state)
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        store.subscribe(this.handleStoreChange)
     }
+
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        };
+        store.dispatch(action);
+    }
+
+    handleStoreChange() {
+        this.setState(store.getState())
+    }
+
+    handleSubmit() {
+        const action = {
+            type: 'add_todo_item'
+        };
+        store.dispatch(action);
+    }
+
     opt = {
         size: 'large'
     };
@@ -15,19 +38,21 @@ export default class TodoList extends Component {
     render() {
         const size = this.opt.size;
         return (
-            <div style={{width:960,margin: '0 auto'}}>
+            <div style={{width: 960, margin: '0 auto'}}>
                 <Input
                     placeholder="todo info"
                     value={this.state.inputValue}
-                    readOnly
                     size={size}
                     style={{width: 500, margin: '50px 0 0 50px'}}
+                    onChange={this.handleInputChange}
                 />
                 <Button
                     type="primary"
-                    size={size}>提交</Button>
+                    size={size}
+                    onClick={this.handleSubmit}
+                >提交</Button>
                 <List
-                    style={{width:500,margin: '10px 0 0 50px'}}
+                    style={{width: 500, margin: '10px 0 0 50px'}}
                     header={<div>Header</div>}
                     footer={<div>Footer</div>}
                     bordered
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,5 +8,16 @@ const defaultState = {
 };
 
 export default (state = defaultState, action) => {
+    if (action.type === 'change_input_value') {
+        const newState = JSON.parse(JSON.stringify(state)); // 深拷贝
+        newState.inputValue = action.value;
+        return newState;
+    }
+    if (action.type === 'add_todo_item'){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        return newState;
+    }
     return state;
 }
