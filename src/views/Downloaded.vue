b914fe6831fe05a0ab3f4c5a209c80270197c8e3
Mrlhz <mr__lhz@163.com>
2022-08-20 23:37:37 +0800

    refactor: modify components using composite APIs

--- src/views/Downloaded.vue
+++ src/views/Downloaded.vue
@@ -1,72 +1,69 @@
-<template>
-  <div class="wrapper" id="images" ref="images">
-    <template v-for="item in list" :key="item.title">
-      <ImageCardItem :imageItem="item" @handleClick="handleClick" />
-    </template>
-  </div>
-</template>
-<script>
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+import { useRouter, onBeforeRouteLeave } from 'vue-router'
+
 import Viewer from 'viewerjs'
 import Request from '../utils/http'
-import { ImageCardItem } from '../components/ImageCard'
+import { ImageCardList } from '../components/ImageCard'
 
-export default {
-  name: 'Downloaded',
-  components: {
-    ImageCardItem,
-  },
-  data() {
-    return {
-      star: 'Downloaded',
-      list: [],
-      viewer: {},
-      scroll: 0,
-    }
-  },
-  created() {
-    document.addEventListener('scroll', this.getClientHeight)
-  },
-  mounted() {
-    console.log(this.$route)
-    this.init()
-  },
-  beforeUnmount() {
-    if (this.viewer.destory) {
-      this.viewer.destory()
-    }
-    this.viewer = {}
-    document.removeEventListener('scroll', this.getClientHeight)
-  },
-  methods: {
-    async init() {
-      await this.getStarList()
-      this.handleViewer()
-      window.scrollTo(0, Number.parseInt(localStorage.getItem('StarListScroll'), 10))
-    },
-    async getStarList() {
-      const params = { star: this.star }
-      const result = await Request.get('/star', { params })
-      console.log(result)
-      this.list = result.starList
-      return result
-    },
-    handleViewer() {
-      this.viewer = new Viewer(document.getElementById('images'))
-    },
-    handleClick(item) {
-      console.log('starList', item)
-      this.$router.push({ name: 'StarDetail', params: { ...item, star: this.star } })
-    },
-    getClientHeight() {
-      this.scroll = document.documentElement.scrollTop || document.body.scrollTop
-    },
-  },
-  beforeRouteLeave(to, from, next) {
-    console.log({ to, from })
-    localStorage.setItem('StarListScroll', this.scroll)
-    next()
-  },
+const router = useRouter()
+
+const star = 'Downloaded'
+const list = ref([])
+const viewer = ref(null)
+const scroll = ref(0)
+
+async function getStarList() {
+  const params = { star }
+  const result = await Request.get('/star', { params })
+  console.log(result)
+  list.value = result.starList
+  return result
 }
+
+async function init() {
+  await getStarList()
+  // this.handleViewer()
+  viewer.value = new Viewer(document.getElementById('images'))
+  window.scrollTo(0, Number.parseInt(localStorage.getItem('StarListScroll'), 10))
+}
+
+function getClientHeight() {
+  return document.documentElement.scrollTop || document.body.scrollTop
+}
+function setClientHeight() {
+  scroll.value = getClientHeight()
+}
+
+onMounted(async () => {
+  document.addEventListener('scroll', setClientHeight)
+
+  await init()
+})
+
+onUnmounted(() => {
+  if (viewer.value.destory) {
+    viewer.value.destory()
+  }
+  viewer.value = {}
+  document.removeEventListener('scroll', setClientHeight)
+})
+
+onBeforeRouteLeave((to, from, next) => {
+  console.log({ to, from })
+  localStorage.setItem('StarListScroll', scroll.value)
+  next()
+})
+
+function handleClick(item) {
+  console.log('starList', item, router)
+  router.push({ name: 'StarDetail', params: { ...item, star } })
+}
+
 </script>
-<style scoped lang="scss">
-</style>
+
+<template>
+  <div class="wrapper" id="images" ref="images">
+    <ImageCardList :list="list" :star="star" @handleItemClick="handleClick" />
+  </div>
+</template>
